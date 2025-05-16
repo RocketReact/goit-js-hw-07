2bc2ab34e2cdc4d340b641c6bdd6d3de5f38d960
Oleg Shapoval <olegkazhan@gmail.com>
2025-05-16 17:11:15 +0300

    5 task

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,17 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+console.log(getRandomHexColor());
+
+const body = document.querySelector('body');
+const span = document.querySelector('.color');
+
+const button = document.querySelector(
+  '.change-color',
+);
+button.addEventListener('click', (e) => {
+  e.preventDefault();
+  body.style.backgroundColor =
+    getRandomHexColor();
+  span.textContent = getRandomHexColor();
+});
