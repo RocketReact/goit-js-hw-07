37c538564e6bb674c69cdd466bb5e0c71f1d92dd
Oleg Shapoval <olegkazhan@gmail.com>
2025-05-16 16:45:46 +0300

    4 task

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,25 @@
+const formSelect = document.querySelector('form');
+const emailInput = document.querySelector(
+  'input[name="email"]',
+);
+const passwordInput = document.querySelector(
+  'input[name="password"]',
+);
 
+formSelect.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const email = emailInput.value.trim();
+  const password = passwordInput.value.trim();
+
+  if (email === '' || password === '') {
+    alert(
+      'Please enter a valid email & password',
+    );
+    return;
+  }
+
+  e.target.submit();
+  console.log({ email, password });
+
+  formSelect.reset();
+});
