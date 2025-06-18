9c958a156e9bcfe18e05210f55ae0d096784d4f8
Oleg Shapoval <olegkazhan@gmail.com>
2025-06-18 10:35:12 +0300

    fix 1.js

--- js/task-1.js
+++ js/task-1.js
@@ -2,9 +2,7 @@ const chooseLi = document.querySelectorAll('.item');
 const countLi = chooseLi.length;
 console.log('Number of categories:', countLi);
 
-const firstH2 = document.querySelectorAll('#categories .item');
-
-firstH2.forEach((item) => {
+chooseLi.forEach((item) => {
   console.log('Category:', item.querySelector('h2').textContent);
-  console.log('Elements:', item.querySelectorAll('ul li').length);
+  console.log('Elements:', item.querySelectorAll('.item>ul li').length);
 });
